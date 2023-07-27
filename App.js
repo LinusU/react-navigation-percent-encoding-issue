9d0cf26fb2b50c33a6ed45c4113d76f9152844dd
Linus Unnebäck <linus@folkdatorn.se>
2023-07-27 12:56:55 +0200

    Add code showing the issue

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react'
+import { View, Text, Button } from 'react-native'
+import { NavigationContainer, useRoute, useNavigation } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+
+function TestScreen() {
+  const params = useRoute().params
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Test Screen</Text>
+      <Text>The id is: {JSON.stringify(params.id)}</Text>
+    </View>
+  )
+}
+
+function HomeScreen() {
+  const navigation = useNavigation()
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+      <Button title='Conduct Test' onPress={() => navigation.navigate('Test', { id: 'test=' })} />
+    </View>
+  )
+}
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer linking={{ config: { screens: { Test: { path: 'test/:id' } } } }}>
+      <Stack.Navigator initialRouteName='Home'>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Test" component={TestScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
